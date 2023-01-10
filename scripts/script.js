b10f6560f5b916780f6c4562aabfa3db7413671d
domitraktor <dominika.wojdyla18@gmail.com>
2023-01-10 14:41:28 +0100

    add support for mobile version

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+
+function isInputValid(inputValue){
+    if(inputValue.length > 8){
+        return true
+    }else{
+        return false
+    }
+}
+
+
+const login = "domiTraktor"
+const password = "Stokrotka1999"
+
+
+// isInputValid(login)
+// isInputValid(password)
+
+if (login.length > 8) {
+    return true
+} else {
+    return false
+}
+
+
+if (password.length > 8) {
+    return true
+} else {
+    return false
+}
