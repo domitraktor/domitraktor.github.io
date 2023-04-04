bfde1cd41416493f8b1c1f0cd89de715f3f5c138
domitraktor <dominika.wojdyla18@gmail.com>
2023-04-04 12:43:32 +0200

    add username and password validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,86 +1,105 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    const email = document.querySelector("#email");
-    const username = document.querySelector("#username");
-    const password = document.querySelector("#password");
-    const form = document.querySelector("form");
+const special = ['`', '!', '@', '#', '$', '^', '&', '*']
+const monkey = '@'
 
-    const special = ["`", "!", "@", "#", "$", "^", "&", "*"];
+function checkIfOneChar(password) {
+    let isOneCharPresent = false
+    special.forEach((char) => {
+        if (password.includes(char)) {
+            isOneCharPresent = true
+        }
+    })
+    return isOneCharPresent
+}
 
-    function checkIfOneChar(password) {
-        let isOneCharPresent = false
-        special.forEach(char => {
-            if (password.includes(char)) {
-                isOneCharPresent = true
-            }
-        })
-        return isOneCharPresent
+function isUpperCase(myString) {
+    return myString == myString.toUpperCase()
+}
+function checkIfUpperCase(passedPassword) {
+    let isOneUpperCased = false
 
-    }
+    passedPassword.split('').forEach((letter) => {
+        if (isUpperCase(letter) == true) {
+            isOneUpperCased = true
+        }
+    })
 
-    function isUpperCase(myString) {
-        return myString == myString.toUpperCase();
-    }
-    function checkIfUpperCase(passedPassword) {
-        let isOneUpperCased = false;
+    return isOneUpperCased
+}
 
-        passedPassword.split("").forEach((letter) => {
-            if (isUpperCase(letter) == true) {
-                isOneUpperCased = true;
-            }
-        });
+document.addEventListener('DOMContentLoaded', function (event) {
+    const email = document.querySelector('#email')
+    const username = document.querySelector('#username')
+    const password = document.querySelector('#password')
+    const form = document.querySelector('form')
 
-        return isOneUpperCased;
-    }
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
+        //1. emailValidation
+        //2. usernameValidation
+        //3. passwordValidation
 
-        const monkey = "@";
         if (email.value.includes(monkey)) {
-            console.log("Ok");
+            console.log('Ok')
+
+            //tutaj mozna usuwac wczesniejszy komunikat o blednym
         } else {
-            console.log("Provide a valid e-mail address");
+            if (document.querySelector('#email-validation-message') === null) {
+                const paragraphAfterEmail = document.createElement('p')
+                paragraphAfterEmail.innerText = 'Provide a valid e-mail address'
+                email.insertAdjacentElement('afterend', paragraphAfterEmail)
+                paragraphAfterEmail.setAttribute(
+                    'id',
+                    'email-validation-message'
+                )
+            }
         }
-
-
-        if (document.querySelector('#email-validation-message') === null) {
-            const paragraphAfterEmail = document.createElement("p")
-            paragraphAfterEmail.innerText = "Provide a valid e-mail address"
-            email.insertAdjacentElement("afterend", paragraphAfterEmail)
-            paragraphAfterEmail.setAttribute("id", "email-validation-message")
-
-        }
-
-
-
 
         if (username.value.length >= 3) {
-            console.log("Ok");
+            console.log('Ok')
         } else {
-            console.log("Username must be longer than two characters");
+            console.log('Username must be longer than two characters')
+        }
+        if (document.querySelector('#username-validation') === null) {
+            if (username.value.length < 3) {
+                const usernameAfterSecondInput = document.createElement('p')
+                usernameAfterSecondInput.innerText =
+                    'Username must be longer than two characters'
+                username.insertAdjacentElement(
+                    'afterend',
+                    usernameAfterSecondInput
+                )
+                usernameAfterSecondInput.setAttribute(
+                    'id',
+                    'username-validation'
+                )
+            }
         }
 
-
-
-
         if (checkIfOneChar(password.value)) {
-            console.log("Okejka")
+            console.log('Okejka')
         } else {
-            console.log("Daj znak specjalny")
+            console.log('Daj znak specjalny')
+        }
+
+        if (document.querySelector('#password-validation') === null) {
+            const passwordWithSpecialChar = document.createElement('p')
+            passwordWithSpecialChar.innerText =
+                'Password must contain one special sign(!,@,#,$,%,^,&,*)'
+            password.insertAdjacentElement('afterend', passwordWithSpecialChar)
+            passwordWithSpecialChar.setAttribute('id', 'password-validation')
         }
 
         if (password.value.length >= 8) {
-            console.log("Okejka")
+            console.log('Okejka')
         } else {
-            console.log("Wpisz min. osiem znaków")
+            console.log('Wpisz min. osiem znaków')
         }
 
         if (checkIfUpperCase(password.value)) {
-            console.log("okejka")
+            console.log('okejka')
         } else {
-            console.log("Wpisz min.jedną dużą literę")
+            console.log('Wpisz min.jedną dużą literę')
         }
-
-
     })
 })
