03e4a4e1bf978778c5dca26a1ae7fd869e35e83e
domitraktor <dominika.wojdyla18@gmail.com>
2023-04-17 12:39:46 +0200

    refactor

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,4 +1,4 @@
-import { special } from "./constants.js"
+import { monkey, special } from "./constants.js"
 
 export const checkIfSpecialSign = (password) => {
     return special.some((char) => password.includes(char))
@@ -18,4 +18,127 @@ export const checkIfUpperCase = (passedPassword) => {
     })
 
     return isOneUpperCased
+}
+
+const cleanupPreviousValidationMsg = (id, inputElement) => {
+    const previousValidationMsgExists = document.querySelector(id) !== null
+    if (previousValidationMsgExists) {
+        document.querySelector(id).remove()
+    }
+    inputElement.classList.remove("validation-error")
+}
+
+export const validateEmail = (emailDOMElement) => {
+    if (emailDOMElement.value.includes(monkey)) {
+        cleanupPreviousValidationMsg(
+            "#email-validation-message",
+            emailDOMElement
+        )
+    } else {
+        cleanupPreviousValidationMsg(
+            "#email-validation-message",
+            emailDOMElement
+        )
+        const paragraphAfterEmail = document.createElement("p")
+        paragraphAfterEmail.innerText = "Provide a valid e-mail address"
+        emailDOMElement.insertAdjacentElement("afterend", paragraphAfterEmail)
+        paragraphAfterEmail.setAttribute("id", "email-validation-message")
+
+        emailDOMElement.classList.add("validation-error")
+    }
+}
+
+export const validateUsername = (usernameDOMElement) => {
+    if (usernameDOMElement.value.length >= 3) {
+        cleanupPreviousValidationMsg(
+            "#username-validation-message",
+            usernameDOMElement
+        )
+    } else {
+        cleanupPreviousValidationMsg(
+            "#username-validation-message",
+            usernameDOMElement
+        )
+        const usernameAfterSecondInput = document.createElement("p")
+        usernameAfterSecondInput.innerText =
+            "Username must be longer than two characters"
+        usernameDOMElement.insertAdjacentElement(
+            "afterend",
+            usernameAfterSecondInput
+        )
+        usernameAfterSecondInput.setAttribute(
+            "id",
+            "username-validation-message"
+        )
+        usernameDOMElement.classList.add("validation-error")
+    }
+}
+
+export const validatePassword = (passwordDOMElement) => {
+    let numberOfErrorsForPassword = 0
+
+    if (checkIfSpecialSign(passwordDOMElement.value)) {
+        cleanupPreviousValidationMsg("#password-validation", passwordDOMElement)
+    } else {
+        cleanupPreviousValidationMsg("#password-validation", passwordDOMElement)
+        numberOfErrorsForPassword++
+        const passwordWithSpecialChar = document.createElement("p")
+        passwordWithSpecialChar.innerText =
+            "Password must contain one special sign(!,@,#,$,%,^,&,*)"
+        passwordDOMElement.insertAdjacentElement(
+            "afterend",
+            passwordWithSpecialChar
+        )
+        passwordWithSpecialChar.setAttribute("id", "password-validation")
+    }
+
+    if (passwordDOMElement.value.length >= 8) {
+        cleanupPreviousValidationMsg(
+            "#passwordLength-validation",
+            passwordDOMElement
+        )
+    } else {
+        cleanupPreviousValidationMsg(
+            "#passwordLength-validation",
+            passwordDOMElement
+        )
+        numberOfErrorsForPassword++
+        const passwordLength = document.createElement("p")
+        passwordLength.innerText =
+            "Password must be at least eight characters long"
+        passwordDOMElement.insertAdjacentElement("afterend", passwordLength)
+        passwordLength.setAttribute("id", "passwordLength-validation")
+    }
+
+    if (checkIfUpperCase(passwordDOMElement.value)) {
+        cleanupPreviousValidationMsg(
+            "#passwordUpperCase-validation",
+            passwordDOMElement
+        )
+    } else {
+        cleanupPreviousValidationMsg(
+            "#passwordUpperCase-validation",
+            passwordDOMElement
+        )
+        numberOfErrorsForPassword++
+        const upperCasePassword = document.createElement("p")
+        upperCasePassword.innerText =
+            "Password must contain at least one upper case letter"
+        passwordDOMElement.insertAdjacentElement("afterend", upperCasePassword)
+        upperCasePassword.setAttribute("id", "passwordUpperCase-validation")
+
+        if (numberOfErrorsForPassword === 0) {
+            passwordDOMElement.classList.remove("validation-error")
+        } else {
+            passwordDOMElement.classList.add("validation-error")
+        }
+    }
+}
+
+export const validateCheckbox = (termsOfServiceCheckboxDOMElement) => {
+    if (termsOfServiceCheckboxDOMElement.checked) {
+        return true
+    } else {
+        return false
+    }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,114 +1,28 @@
-import { monkey } from "./constants.js"
-import { checkIfSpecialSign, checkIfUpperCase } from "./helpers.js"
+import {
+    validateEmail,
+    validateUsername,
+    validatePassword,
+    validateCheckbox,
+} from "./helpers.js"
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    const email = document.querySelector("#email")
-    const username = document.querySelector("#username")
-    const password = document.querySelector("#password")
+document.addEventListener("DOMContentLoaded", function () {
+    const emailDOMElement = document.querySelector("#email")
+    const usernameDOMElement = document.querySelector("#username")
+    const passwordDOMElement = document.querySelector("#password")
     const form = document.querySelector("form")
-    const termsOfServiceCheckbox = document.querySelector(
+    const termsOfServiceCheckboxDOMElement = document.querySelector(
         "#termsOfServiceCheckbox"
     )
 
     form.addEventListener("submit", (event) => {
         event.preventDefault()
 
-        const cleanupPreviousValidationMsg = (id, inputElement) => {
-            const previousValidationMsgExists =
-                document.querySelector(id) !== null
-            if (previousValidationMsgExists) {
-                document.querySelector(id).remove()
-            }
-            inputElement.classList.remove("validation-error")
-        }
+        validateEmail(emailDOMElement)
 
-        if (email.value.includes(monkey)) {
-            cleanupPreviousValidationMsg("#email-validation-message", email)
-        } else {
-            cleanupPreviousValidationMsg("#email-validation-message", email)
-            const paragraphAfterEmail = document.createElement("p")
-            paragraphAfterEmail.innerText = "Provide a valid e-mail address"
-            email.insertAdjacentElement("afterend", paragraphAfterEmail)
-            paragraphAfterEmail.setAttribute("id", "email-validation-message")
+        validateUsername(usernameDOMElement)
 
-            email.classList.add("validation-error")
-        }
+        validatePassword(passwordDOMElement)
 
-        if (username.value.length >= 3) {
-            cleanupPreviousValidationMsg(
-                "#username-validation-message",
-                username
-            )
-        } else {
-            cleanupPreviousValidationMsg(
-                "#username-validation-message",
-                username
-            )
-            const usernameAfterSecondInput = document.createElement("p")
-            usernameAfterSecondInput.innerText =
-                "Username must be longer than two characters"
-            username.insertAdjacentElement("afterend", usernameAfterSecondInput)
-            usernameAfterSecondInput.setAttribute(
-                "id",
-                "username-validation-message"
-            )
-            username.classList.add("validation-error")
-        }
-
-        let numberOfErrorsForPassword = 0
-
-        if (checkIfSpecialSign(password.value)) {
-            cleanupPreviousValidationMsg("#password-validation", password)
-        } else {
-            cleanupPreviousValidationMsg("#password-validation", password)
-            numberOfErrorsForPassword++
-            const passwordWithSpecialChar = document.createElement("p")
-            passwordWithSpecialChar.innerText =
-                "Password must contain one special sign(!,@,#,$,%,^,&,*)"
-            password.insertAdjacentElement("afterend", passwordWithSpecialChar)
-            passwordWithSpecialChar.setAttribute("id", "password-validation")
-        }
-
-        if (password.value.length >= 8) {
-            cleanupPreviousValidationMsg("#passwordLength-validation", password)
-        } else {
-            cleanupPreviousValidationMsg("#passwordLength-validation", password)
-            numberOfErrorsForPassword++
-            const passwordLength = document.createElement("p")
-            passwordLength.innerText =
-                "Password must be at least eight characters long"
-            password.insertAdjacentElement("afterend", passwordLength)
-            passwordLength.setAttribute("id", "passwordLength-validation")
-        }
-
-        if (checkIfUpperCase(password.value)) {
-            cleanupPreviousValidationMsg(
-                "#passwordUpperCase-validation",
-                password
-            )
-        } else {
-            cleanupPreviousValidationMsg(
-                "#passwordUpperCase-validation",
-                password
-            )
-            numberOfErrorsForPassword++
-            const upperCasePassword = document.createElement("p")
-            upperCasePassword.innerText =
-                "Password must contain at least one upper case letter"
-            password.insertAdjacentElement("afterend", upperCasePassword)
-            upperCasePassword.setAttribute("id", "passwordUpperCase-validation")
-        }
-
-        if (numberOfErrorsForPassword === 0) {
-            password.classList.remove("validation-error")
-        } else {
-            password.classList.add("validation-error")
-        }
-
-        if (termsOfServiceCheckbox.checked) {
-            return true
-        } else {
-            return false
-        }
+        validateCheckbox(termsOfServiceCheckboxDOMElement)
     })
 })
